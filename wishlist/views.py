4647067190b362405a0bdfbef6b5d3c63f342c9e
Dhiwa Arya Kusumah <dhiwaarya@Dhiwas-MacBook-Pro.local>
2022-09-08 12:04:00 +0700

    Lab 1

---
+++ wishlist/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from wishlist.models import ItemWishlist
+
+# Create your views here.
+
+def show_wishlist(request):
+    data_wishlist_item = ItemWishlist.objects.all()
+    context = {
+        'list_item': data_wishlist_item,
+        'name': 'Dhiwa Arya Kusumah'
+    }
+    return render(request, "wishlist.html", context)
+
+
